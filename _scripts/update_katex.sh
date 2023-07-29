acea1882ba5b3a6e451bc9fa2c7336e779ddccf2
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-07-29 23:54:07 +0800

    update fonts
    + bump katex to v0.16.8
    + update open sans google font
    + remove clarity city and source serif pro from webfonts
    + zipped fonts instead of individual font files under downlaods
    + do cleanups after update in update_katex script

--- _scripts/update_katex.sh
+++ _scripts/update_katex.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+# run this script from the root drive folder
+# bash _scripts/update_katex.sh
 sudo apt update
-sudo apt install -y --no-install-recommends \
-    unzip \
-    wget
+sudo apt install -y --no-install-recommends unzip wget
 CWD="${PWD}"
 cd /tmp
-if [ -f "katex.zip" ] ; then
-    rm katex.zip
-fi
-if [ -d "katex" ] ; then
-    rm -rf katex
-fi
 wget https://github.com/katex/katex/releases/latest/download/katex.zip
 unzip katex.zip
+rm katex.zip
 cp katex/katex.min.css ${CWD}/webfonts/katex
 cp katex/katex.min.js ${CWD}/webfonts/katex
 cp -r katex/fonts ${CWD}/webfonts/katex
+rm -rf katex
+cd ${CWD}
